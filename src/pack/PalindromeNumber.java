0016adbd9f0bd1c7c0843b7e2cf277711de5c721
CherrY <charitha2301@gmail.com>
2026-05-09 18:58:15 +0530

    MODIFIED MESSAGE

--- src/pack/PalindromeNumber.java
+++ src/pack/PalindromeNumber.java
@@ -1,9 +1,24 @@
+
 package pack;
 
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-		System.out.println("write the code.....");
-	}
+		
+		int n = 45654;
+		int temp = n;
+		int rev = 0;
 
+		while (n != 0) {
+			int rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+
+		if (temp== rev)
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+
+	}
 }
