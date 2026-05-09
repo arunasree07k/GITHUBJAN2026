84d3c9bb83f37c4595cd8395922e09ec5425966a
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-05-09 18:56:25 +0530

    modified file.....

--- src/pack/PalindromeNumber.java
+++ src/pack/PalindromeNumber.java
@@ -3,7 +3,10 @@ package pack;
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-		System.out.println("write the code.....");
+		Scanner sc = new Scanner(System.in) {
+			
+		}
+		System.out.println("it's not a palindrome");
 	}
 
 }
