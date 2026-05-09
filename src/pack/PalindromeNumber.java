b518a91128bb83a285efe78cca5c6eb44939adcd
Bhavitha <234g1a0524@srit.ac.in>
2026-05-09 18:58:41 +0530

    modified file......

--- src/pack/PalindromeNumber.java
+++ src/pack/PalindromeNumber.java
@@ -4,6 +4,18 @@ public class PalindromeNumber {
 
 	public static void main(String[] args) {
 		System.out.println("write the code.....");
+		int num=12321;
+		int temp=num;
+		int p=0;
+		while(num!=0) {
+			int rev=num%10;
+		    p=(p*10)+rev;
+		    num/=10;
+		}
+		if(temp==p)
+		System.out.println("Number  Palindrome");
+		else
+		System.out.println("Number is Not a Palindrome");
 	}
 
 }
